67a7a98322ac781aa70e26b61ae5c9e39474a433
Bryce Kennedy <btken88@gmail.com>
2020-07-15 14:55:37 -0600

    Add event handler to change state

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,14 +3,33 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    people: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 30 },
+      { name: 'Stephanie', age: 26 },
+    ]
+  }
+
+  switchNameHandler = () => {
+    this.setState({
+      people: [
+        { name: 'Maximus', age: 28 },
+        { name: 'Manuda', age: 30 },
+        { name: 'Stephan', age: 26 },
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React app!</h1>
         <p>This is totally working!</p>
-        <Person name="Max" age="28" />
-        <Person name="Manu" age="30">My hobbies are: .........</Person>
-        <Person name="Stephanie" age="29" />
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.people[0].name} age={this.state.people[0].age} />
+        <Person name={this.state.people[1].name} age={this.state.people[1].age}>My hobbies are: .........</Person>
+        <Person name={this.state.people[2].name} age={this.state.people[2].age} />
       </div>
     );
   }
